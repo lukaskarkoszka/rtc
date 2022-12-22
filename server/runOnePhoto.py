cdc21448f92aadc12978a6c3404c6e8231b87aa1
Michal <mkrol2k@gmail.com>
2022-12-22 09:37:38 +0100

    switched back to globals, handled CLICK, assigned tracker bbox to global BBOX

--- server/runOnePhoto.py
+++ server/runOnePhoto.py
@@ -1,11 +1,19 @@
 from objectDetection import objectDetection
 import cv2
 
-img=cv2.imread('ludzie.jpg')
-
 objectDetection = objectDetection()
-img, detections = objectDetection.detection(img)
-print(detections)
-cv2.imshow("CSI Camera", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def testDevice(source):
+   cap = cv2.VideoCapture()
+   cap.open(source, cv2.CAP_DSHOW)
+   if cap is None or not cap.isOpened():
+       print('Warning: unable to open video source: ', source)
+   else:
+       print('OK: ', source)
+       while (True):
+           ret, frame = cap.read()
+           img, detections = objectDetection.detection(frame)
+           if cv2.waitKey(1) & 0xFF == ord('q'):
+               break
+           print(detections)
+testDevice(0)
